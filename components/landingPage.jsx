dc3b47785f47696424f8ce6af4e159fbd00dd2e7
Eric Cheng <ericcheng9316@gmail.com>
2022-01-02 15:17:11 -0500

    fixes

--- components/landingPage.jsx
+++ components/landingPage.jsx
@@ -2,19 +2,17 @@ import React from 'react'
 import { Box, Container, Text} from '@chakra-ui/react'
 import Button from '@mui/material/Button'
 import TypeIt from "typeit-react";
-import 'styles/globals.css'
-import 'styles/Home.module.css'
 
 export const LandingPage = () => {
     return (
-        <Box bg="black" color="white">
-            <Box id='welcome-typeit'>
+        <Box bg="black" color="white" className="BlinkMacSystemFont">
+            <Box id='welcome-typeit' class='text-3xl font-mono BlinkMacSystemFont'>
                 <Container className='container'>
-                    <TypeIt className='title'>Hi, I&#39;m Eric.</TypeIt>
+                    <TypeIt className='title BlinkMacSystemFont'>Hi, I&#39;m Eric.</TypeIt>
                 </Container>
             </Box>
 
-            <Box className='main-content'>
+            <Box className='main-content BlinkMacSystemFont'>
                 <Container className='container'>
                     <Text className='main'>I&#39;m a high school sophomore at Green Hope High School.
                     Since discovering programming in 9th grade, coding has fascinated me with how simple, yet complex it is.
@@ -25,9 +23,9 @@ export const LandingPage = () => {
                 </Container>
             </Box>
 
-            <Box bg="black" color="white" className='contact-buttons'>
-                <Button href='https://github.com/import-brain' variant='contained' color='primary'>GitHub</Button>
-                <Button href='mailto:ericcheng9316@gmail.com' variant='contained' color='primary'>Email</Button>
+            <Box bg="black" color="white" className='contact-buttons BlinkMacSystemFont'>
+                <Button href='https://github.com/import-brain' variant='contained' className='orange'>GitHub</Button>
+                <Button href='mailto:ericcheng9316@gmail.com' variant='contained' className='orange'>Email</Button>
             </Box>
         </Box>
     )
