c3987dcad0fa2584cf9a543c5562b72416aa77c1
Eric Cheng <ericcheng9316@gmail.com>
2022-01-02 13:48:34 -0500

    buffed up landing page content

--- components/landingPage.jsx
+++ components/landingPage.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
-import { Box, Container } from '@chakra-ui/react'
+import { Box, Container, Text} from '@chakra-ui/react'
 import Button from '@mui/material/Button'
 import TypeIt from "typeit-react";
+import 'styles/globals.css'
+import 'styles/Home.module.css'
 
 export const LandingPage = () => {
     return (
-        <Box bg="black" color="white" className='page'>
+        <Box bg="black" color="white">
             <Box id='welcome-typeit'>
-                <Container className='welcome-text'>
-                    <TypeIt>Hi, I&#39;m Eric.</TypeIt>
+                <Container className='container'>
+                    <TypeIt className='title'>Hi, I&#39;m Eric.</TypeIt>
                 </Container>
             </Box>
 
             <Box className='main-content'>
-                <Container className='main-text'>
-                    <Button variant='contained'>test</Button>
+                <Container className='container'>
+                    <Text className='main'>I&#39;m a high school sophomore at Green Hope High School.
+                    Since discovering programming in 9th grade, coding has fascinated me with how simple, yet complex it is.
+                    In my free time, I like to create applications that have a positive impact on the lives of those around me.
+                    I&#39;ve created/contributed to programs and services as varied as websites, Python modules, JavaScript libraries, and more.
+                    Feel free to connect with me below!
+                    </Text>
                 </Container>
             </Box>
 
