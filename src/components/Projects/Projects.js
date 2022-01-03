84ce9e5a23bd67a7c5aafd62ad6558919b8fd8a7
Eric Cheng <ericcheng9316@gmail.com>
2022-01-03 08:23:29 -0500

    scaffolding for projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,35 @@
 import React from 'react';
 
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { Section, SectionDivider, SectionTitle, SectionTitleTyped } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
+import TypeIt from 'typeit-react';
+
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitleTyped main>
+      <TypeIt>Projects I've Worked On</TypeIt>
+    </SectionTitleTyped>
+    <GridContainer>
+      {projects.map((project) => (
+        <div>
+          {project.image}
+          <br />
+          {project.title}
+          <br />
+          {project.description}
+          <br />
+          {project.techUsed}
+          <br />
+          {project.visit}
+          <br />
+          {project.source}
+        </div>
+      ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
