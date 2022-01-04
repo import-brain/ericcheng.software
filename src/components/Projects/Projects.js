4934025953e740f4515ad39b391a84651baee24c
Eric Cheng <ericcheng9316@gmail.com>
2022-01-04 00:40:37 -0500

    add stack, fix stack spacing in projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionTitle, SectionTitleTyped } from '../../styles/GlobalComponents';
+import { Section, SectionDivider, SectionTitleTyped } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
 import TypeIt from 'typeit-react';
@@ -13,16 +13,35 @@ const Projects = () => (
       <TypeIt>Projects I've Worked On</TypeIt>
     </SectionTitleTyped>
     <GridContainer>
-      {projects.map((project) => (
-        <BlogCard key={project.id}>
-          <Img src={project.image} />
-          <TitleContent>
-            <HeaderThree>{project.title}</HeaderThree>
+      {projects.map((p, i) => {
+        return (
+          <BlogCard key={i}>
+          <Img src={p.image} />
+            <TitleContent>
+              <HeaderThree title>{p.title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo className="card-info">{p.description}</CardInfo>
             <Hr />
-          </TitleContent>
-          <CardInfo>{project.description}</CardInfo>
-        </BlogCard>
-      ))}
+            <div>
+              <TitleContent>Stack</TitleContent>
+              <TagList>
+                {p.tags.map((t, i) => {
+                  return (
+                    <div>
+                      <Tag>{t}</Tag>
+                    </div>
+                  );
+                })}
+              </TagList>
+            </div>
+            <UtilityList>
+              <ExternalLinks href={p.visit}>Website</ExternalLinks>
+              <ExternalLinks href={p.source}>Source Code</ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+        );
+      })}
     </GridContainer>
   </Section>
 );
--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -26,7 +26,7 @@ export const BlogCard = styled.div`
   border-radius: 10px;
   box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
   text-align: center;
-  width: 400px;
+  width: 450px;
   @media ${(props) => props.theme.breakpoints.sm} {
     width: 100%;
   }
