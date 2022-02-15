a0def5c5d79456440f1bccd97aa60d361425cde5
Eric Cheng <ericcheng9316@gmail.com>
2022-02-14 22:27:15 -0500

    edit Hero text

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,7 @@ const Hero = () => (
         <TypeIt>Hi, I'm Eric.</TypeIt>
       </SectionTitleTyped>
       <SectionText>
-        A passionate high-schooler interested in full-stack web development.
+        A passionate high-schooler interested in full-stack web development and open-source software.
       </SectionText>
       <Button onClick={() => window.location = 'mailto:ericcheng9316@gmail.com'}>
         Send me an email
