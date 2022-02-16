535e1c34a1dc65010ae34f172e5b673ef5458d28
Eric Cheng <ericcheng9316@gmail.com>
2022-02-16 14:17:28 -0500

    fix button

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -89,11 +89,11 @@ export const UtilityList = styled.ul`
 `;
 
 export const ExternalLinks = styled.a`
-color:#d4c0c0;
+color: #D4C0C0;
 font-size: 1.6rem;
 font-weight: 400;
-padding:1rem 1.5rem;
-background: #6b3030;
+padding: 1rem 1.5rem;
+background: #6B3030;
 border-radius: 15px;
 transition: 0.5s;
 cursor: pointer;
