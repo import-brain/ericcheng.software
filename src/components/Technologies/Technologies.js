a55929f217aa3841edc3a8a291fc6f19950730c3
Eric Cheng <ericcheng9316@gmail.com>
2022-05-18 13:11:45 -0400

    update tech stack

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,18 +4,22 @@ import { Html5 } from '@styled-icons/boxicons-logos/Html5'
 import { Css3 } from '@styled-icons/boxicons-logos/Css3'
 import { JsSquare } from '@styled-icons/fa-brands/JsSquare'
 import { Typescript } from '@styled-icons/simple-icons/Typescript'
+import { Nextdotjs } from '@styled-icons/simple-icons/Nextdotjs'
 import { ReactLogo } from '@styled-icons/fa-brands/ReactLogo'
 import { Tailwindcss } from '@styled-icons/simple-icons/Tailwindcss'
 
 import { Node } from '@styled-icons/fa-brands/Node'
-import { Express } from '@styled-icons/simple-icons/Express'
+import { Graphql } from '@styled-icons/simple-icons/Graphql'
 
 import { Linux } from '@styled-icons/fa-brands/Linux'
 import { Gnubash } from '@styled-icons/simple-icons/Gnubash'
 import { Git } from '@styled-icons/boxicons-logos/Git'
 import { Github } from '@styled-icons/boxicons-logos/Github'
-import { Nextdotjs } from '@styled-icons/simple-icons/Nextdotjs'
+import { Vercel } from '@styled-icons/simple-icons/Vercel'
+import { Heroku } from '@styled-icons/simple-icons/Heroku'
+import { Netlify } from '@styled-icons/simple-icons/Netlify'
 import { Circleci } from '@styled-icons/simple-icons/Circleci'
+import { Postman } from '@styled-icons/simple-icons/Postman'
 
 import { Python } from '@styled-icons/boxicons-logos/Python'
 import { Java } from '@styled-icons/fa-brands/Java'
@@ -40,6 +44,7 @@ const Technologies = () => (
         <Css3 size='4.1rem' className="pad-inline"/>
         <JsSquare size='4rem' className="pad-inline"/>
         <Typescript size='3.45rem' className="pad-inline"/>
+        <Nextdotjs size='3.45rem' className="pad-inline"/>
         <ReactLogo size='4rem' className="pad-inline"/>
         <Tailwindcss size='4rem' className="pad-inline"/>
       </ListItem>
@@ -49,7 +54,7 @@ const Technologies = () => (
           Back-End
         </SectionText>
         <Node size='4rem' className="pad-inline"/>
-        <Express size='4rem' className="pad-inline"/>
+        <Graphql size='4rem' className="pad-inline"/>
       </ListItem>
 
       <ListItem>
@@ -60,8 +65,11 @@ const Technologies = () => (
         <Gnubash size='4rem' className="pad-inline"/>
         <Git size='4rem' className="pad-inline"/>
         <Github size='4rem' className="pad-inline"/>
-        <Nextdotjs size='4rem' className="pad-inline"/>
+        <Vercel size='4rem' className="pad-inline"/>
+        <Heroku size='4rem' className="pad-inline"/>
+        <Netlify size='4rem' className="pad-inline"/>
         <Circleci size='4rem' className="pad-inline"/>
+        <Postman size='4rem' className="pad-inline"/>
       </ListItem>
 
       <ListItem>
